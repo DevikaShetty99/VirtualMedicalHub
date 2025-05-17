a1dc9b2d2d9d6c9dbc2121572354635c5b9f1417
Uday Vikram Singh <uday.vikram.singh@sap.com>
2025-05-18 00:02:22 +0530

    frontend

--- src/main/java/com/example/medical/config/SwaggerConfig.java
+++ src/main/java/com/example/medical/config/SwaggerConfig.java
@@ -3,18 +3,13 @@ package com.example.medical.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
-
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
-import org.springframework.context.annotation.Configuration;
-
 @Configuration
 public class SwaggerConfig {
-
     @Bean
     public OpenAPI apiInfo() {
         return new OpenAPI()
